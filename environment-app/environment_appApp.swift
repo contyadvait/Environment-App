ff926466515622b49ad38fc16770af87edb6ee77
contyadvait <advaitthepro@outlook.com>
2024-12-19 15:55:53 +0800

    Initial Commit

---
+++ environment-app/environment_appApp.swift
@@ -0,0 +1,17 @@
+//
+//  environment_appApp.swift
+//  environment-app
+//
+//  Created by Milind Contractor on 19/12/24.
+//
+
+import SwiftUI
+
+@main
+struct environment_appApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
